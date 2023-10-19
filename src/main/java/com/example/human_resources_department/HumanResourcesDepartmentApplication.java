0857802fdee10dcb19e65124f5fd5c05d44195b7
RoMANzhula <roman1985al@ukr.net>
2023-10-19 19:16:09 +0300

    Initial commit

---
+++ src/main/java/com/example/human_resources_department/HumanResourcesDepartmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.human_resources_department;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcesDepartmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumanResourcesDepartmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/human_resources_department/HumanResourcesDepartmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.human_resources_department;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesDepartmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
