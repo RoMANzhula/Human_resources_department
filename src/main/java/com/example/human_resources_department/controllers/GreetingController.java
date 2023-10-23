7d5087def262b0f57b83fa6f2750e1fb805c78e8
RoMANzhula <roman1985al@ukr.net>
2023-10-23 23:49:30 +0300

    This commit has the following additions: 1. Template marker FreeMarker 2. Two template files:   2.1. Greeting (from Spring Guides)   2.2. Main 3. GreetingController

---
+++ src/main/java/com/example/human_resources_department/controllers/GreetingController.java
@@ -0,0 +1,31 @@
+package com.example.human_resources_department.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(
+            @RequestParam(name="name", required=false, defaultValue="World")
+            String name,
+            Model model
+    ) {
+        model.addAttribute("name", name);
+
+        return "greeting";
+    }
+
+    @GetMapping("/main")
+    public String main(
+            Model model
+    ) {
+        model.addAttribute("nameCompany", "Good Big Company Family");
+
+        return "main";
+    }
+
+}
