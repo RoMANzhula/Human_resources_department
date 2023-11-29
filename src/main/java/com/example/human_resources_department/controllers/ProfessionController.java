8302404300d2cdd41e7b58cc9be16ac2f84a3d2f
RoMANzhula <roman1985al@ukr.net>
2023-11-29 22:25:25 +0200

    This commit has the following additions: 1. Template professions - add in table new column with firs and lastname of coworkers as link to their page with info.

--- src/main/java/com/example/human_resources_department/controllers/ProfessionController.java
+++ src/main/java/com/example/human_resources_department/controllers/ProfessionController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,14 +25,20 @@ public class ProfessionController {
         List<Employee> employees = employeeRepository.findAll();
 
         Map<String, Integer> professionCounts = new HashMap<>();
+        Map<String, List<Employee>> coworkersByRole = new HashMap<>();
+
         for (Employee employee : employees) {
             for (Role role : employee.getEmployeeRoles()) {
                 String roleName = role.name();
                 professionCounts.put(roleName, professionCounts.getOrDefault(roleName, 0) + 1);
+
+                coworkersByRole.computeIfAbsent(roleName, k -> new ArrayList<>()).add(employee);
             }
         }
 
         model.addAttribute("professionCounts", professionCounts);
+        model.addAttribute("coworkersByRole", coworkersByRole);
+
         return "professions";
     }
 }
