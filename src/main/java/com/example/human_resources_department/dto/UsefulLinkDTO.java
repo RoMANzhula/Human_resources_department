07986e20cf9cfa93607807ddd115f52a7a691dfd
RoMANzhula <roman1985al@ukr.net>
2024-01-01 16:39:49 +0200

    This commit has the following additions: 1. UsefulLinkDTO.

---
+++ src/main/java/com/example/human_resources_department/dto/UsefulLinkDTO.java
@@ -0,0 +1,43 @@
+package com.example.human_resources_department.dto;
+
+public class UsefulLinkDTO {
+    private Long id;
+    private String name;
+    private String link;
+    private Long ownerId;
+
+    public UsefulLinkDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+}
